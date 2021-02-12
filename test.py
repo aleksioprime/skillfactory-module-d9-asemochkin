4c0387fdf4aea809ed21b949cced14793dd5ff61
aleksioprime <57744848+aleksioprime@users.noreply.github.com>
2021-02-13 00:23:42 +0300

    del test

--- test.py
+++
@@ -1,5 +0,0 @@
-import requests
-new_cat = {'name':'skillfactory-check-1'}
-new_post = {'title':'Название поста', 'status':'P','content':'Содержание поста', 'category':1}
-requests.post('http://127.0.0.1:8000/categories/', data=new_cat)
-requests.post('http://127.0.0.1:8000/', data=new_post)
