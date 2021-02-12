86218b12ea91e32d64905dca322e054f5d16c5b4
aleksioprime <57744848+aleksioprime@users.noreply.github.com>
2021-02-12 23:56:45 +0300

    make it better

--- d9_hw/settings.py
+++ d9_hw/settings.py
@@ -124,8 +124,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
